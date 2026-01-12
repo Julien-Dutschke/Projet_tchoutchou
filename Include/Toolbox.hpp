fdc6400417a59e296ccd7746ed939bfbff4798d2
Matthieu Sauer <matthieu.sauer@epitech.eu>
2026-01-12 21:10:06 +0100

    bugs in parser now fixed. moved a file that I forgot to move previously split now splits and know to take doubled quotes and ignore separator inside. corrected small mistake in test data

--- Include/Toolbox.hpp
+++ Include/Toolbox.hpp
@@ -2,16 +2,27 @@
 #include <string_view>
 #include <concepts>
 #include <vector>
+#include "print.hpp"
 
 namespace toolbox {
 [[nodiscard]] inline std::string_view _default_split_caller(std::string_view s) { return s; }
+
+inline size_t find_unquoted(std::string_view str, std::string_view delim, size_t start)
+{
+	size_t first = str.find(delim, start);
+	size_t second = str.find("\"", start);
+	// size_t third = str.find("'", start);
+	if (first < second || second == str.npos)
+		return first;
+	return str.find('"', second + 1) + 1;
+}
 
 template<std::invocable<std::string_view> F = decltype(_default_split_caller)>
 [[nodiscard]] constexpr std::vector<decltype(std::declval<F>()(std::string_view{""}))> split(std::string_view str, std::string_view delim = " ", F call = [](auto s) { return s; })
 {
    std::vector<decltype(call(str))> ret;
-   size_t start = 0, end = str.find(delim, start);
-   for (; start < str.size(); start = end + delim.size(), end = str.find(delim, start)) {
+   size_t start = 0, end = find_unquoted(str, delim, start);
+   for (; start < str.size(); start = end + delim.size(), end = find_unquoted(str, delim, start)) {
        if (start == end) continue;
        if (end == str.npos) end = str.size();
        ret.emplace_back(call(str.substr(start, end - start)));
--- Source/Parser.cpp
+++ Source/Parser.cpp
@@ -1,7 +1,9 @@
 #include <span>
 #include <stdexcept>
+#include <iomanip>
 #include "Parser.hpp"
 #include "Toolbox.hpp"
+#include "Train.hpp"
 #include "print.hpp"
 
 namespace parser {
@@ -47,8 +49,8 @@ namespace parser {
 			throw std::invalid_argument("invalid format for departure time: " + std::string{vec[7]});
 		if (stop.size() != 2)
 			throw std::invalid_argument("invalid format for stop duration: " + std::string{vec[8]});
-		uint32_t departure = std::stoul(start[0]) + std::stoul(start[1]);
-		uint32_t stopTime = std::stoul(stop[0]) + std::stoul(stop[1]);
+		uint32_t departure = std::stoul(start[0]) * 60 + std::stoul(start[1]);
+		uint32_t stopTime = std::stoul(stop[0]) * 60 + std::stoul(stop[1]);
 		infos.trains.emplace_back(std::string{vec[0]}, std::stoul(std::string{vec[1]}), std::stod(std::string{vec[2]}), std::stod(std::string{vec[3]}),
 		                          std::stod(std::string{vec[4]}), std::string{vec[5]}, std::string{vec[6]}, departure, stopTime);
 	}
@@ -67,7 +69,6 @@ namespace parser {
 			if (line.empty())
 				continue;
 
-			print::info(line);
 			// split the current line to get needed things.
 			auto vec = toolbox::split(line);
 
@@ -81,6 +82,23 @@ namespace parser {
 			else
 				parseTrain(line, std::span(vec), ret);
 		}
+		auto w5 = std::setw(5);
+		auto w10 = std::setw(10);
+		auto w30 = std::setw(30);
+		
+		print::info("Nodes:");
+		for (auto &node : ret.nodes)
+			print::info("\tname: ", w10, node.name);
+		print::info("Rails:");
+		for (auto &rail : ret.rails)
+			print::info("\tnode 1: ", w10, rail.node1, " node 2: ", w10, rail.node2, " length: ", w5, rail.length, " max speed: ", w5, rail.speedLimit);
+		print::info("Events:");
+		for (auto &event : ret.events)
+			print::info("\tname: ", w30, event.name, " probability: ", w5, event.probability, " affected node: ", w10, event.affectedNode, " duration: ", w5, event.duration);
+		print::info("Trains:");
+		for (auto &train : ret.trains)
+			print::info("\tname: ", w10, train.name, " weight: ", w5, train.weight, " friction: ", w5, train.friction, " acceleration: ", w5, train.maxAcceleration, " brake: ", train.maxBreakForce,
+				" departure: ", w10, train.departureNode, " arrival: ", w10, train.arrivalNode, " departure time: ", w5, train.departureTime, " stop duration: ", w5, train.stopDuration);
 		return ret;
 	}
 }
